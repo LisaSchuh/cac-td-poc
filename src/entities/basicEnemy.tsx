304561e0e380dbd3ace8f6896ce019ce41468850
Lisa Schuh <schuh.lisa@outlook.com>
2020-05-01 22:44:47 +0200

    nicht begeistert

--- src/entities/basicEnemy.tsx
+++ src/entities/basicEnemy.tsx
@@ -29,7 +29,7 @@ function VBasicEnemy(props: IPosition) {
       // (!) Konva specific method, it is very important
       context.fillStrokeShape(shape);
     },
-    fill: "#5c2c6d ",
+    fill: "#f946AB ",
     strokeWidth: 1,
     stroke: "#f946AB",
     cornerRadius: 2,
