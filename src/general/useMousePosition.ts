142476ad627c66d712b842f91f368cd2579c764d
Lisa Schuh <schuh.lisa@outlook.com>
2020-08-13 19:56:05 +0200

    try fix mobile click

--- src/general/useMousePosition.ts
+++ src/general/useMousePosition.ts
@@ -1,4 +1,5 @@
 import { IPosition } from "./types";
+import { sendLogEvent } from "./events";
 
 let position = { x: 0, y: 0 };
 document.addEventListener(
@@ -8,14 +9,15 @@ document.addEventListener(
       position = { x: e.clientX, y: e.clientY };
     };
     const setFromTouchEvent = (e: TouchEvent) => {
+      sendLogEvent(`touchmove ${e.touches[0].clientX}`);
       position = { x: e.touches[0].clientX, y: e.touches[0].clientX };
     };
     window.addEventListener("touchmove", setFromTouchEvent);
-    window.addEventListener("touchstart", setFromTouchEvent);
+    // window.addEventListener("touchstart", setFromTouchEvent);
     window.addEventListener("mousemove", setFromEvent);
     return () => {
       window.removeEventListener("mousemove", setFromEvent);
-      window.removeEventListener("touchstart", setFromTouchEvent);
+      // window.removeEventListener("touchstart", setFromTouchEvent);
       window.removeEventListener("touchmove", setFromTouchEvent);
     };
   },
